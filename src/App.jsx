8ea76d84c397745c0004dddbe8bb754bc9073aa6
Nathalia Serna <nathaliasernas@gmail.com>
2022-12-08 15:12:09 -0500

    creacion de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-
+import { Route, Routes } from 'react-router-dom';
+import  Home  from './Routes/Home';
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Contact from './Routes/Contact';
+import Detail from './Routes/Detail';
+import Favs from './Routes/Favs';
 
 
 function App() {
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+    <div className="App">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/dentist/ :id"  element={<Detail/>}/>
+      <Route path="/contacto"  element = {<Contact />} />
+      <Route path="/favs"  element={<Favs />} />
+    </Routes>
+    <Footer />
+  </div>
+   
   );
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
@@ -6,7 +7,15 @@ const Navbar = () => {
 
   return (
     <nav>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
+      <ul>
+        
+          <NavLink to="/">Home</NavLink>
+          <NavLink to='/contacto'>Contacto</NavLink>
+          <NavLink to='/favs'>Favoritos</NavLink>
+      
+        
+      </ul>
+     
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
       <button>Change theme</button>
     </nav>
